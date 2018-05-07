31e24af1240b39cd457e195f7087e2cdf8e2b7c3
ebrahim <eshahrivar@clearpath.ai>
2018-05-06 21:47:45 -0400

    Dagger

--- hw1/run_expert.py
+++ hw1/run_expert.py
@@ -15,6 +15,7 @@ import tf_util
 import gym
 import load_policy
 
+from keras.models import load_model
 from keras.models import Sequential
 from keras.layers import Dense
 import keras
@@ -23,76 +24,89 @@ from keras.wrappers.scikit_learn import KerasRegressor
 def env_dims(env):
     return (env.observation_space.shape[0], env.action_space.shape[0])
 
+class policy:
+    def __init__(self, envname,trained=0):
+        self.trainedBefore=trained
+        self.envname=envname
+        self.env = gym.make(envname)
+        self.state_len, self.action_len = env_dims(self.env)
+        self.model = Sequential()
+        self.model.add(Dense(units=32, input_dim=self.state_len, kernel_initializer='normal', activation='relu'))
+        self.model.add(Dense(units=16, kernel_initializer='normal', activation='relu'))
+        self.model.add(Dense(units=self.action_len, kernel_initializer='normal'))
 
-def policy(X,Y, envname):
-    import gym
-    env = gym.make(envname)
-    state_len, action_len = env_dims(env)
-    model = Sequential()
-    model.add(Dense(state_len, input_dim=11, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(action_len, kernel_initializer='normal'))
+        self.model.compile(loss='mean_squared_error', optimizer='adam')
 
-    model.summary()
+    def train(self, X,Y, epchs=10, bch_size=50):
 
-    tbCallBack = keras.callbacks.TensorBoard(log_dir='/tmp/keras_logs', write_graph=True)
-    # Compile model
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(X, Y, epochs=9, batch_size=50,  verbose=1, validation_split=0.3, callbacks=[tbCallBack])
-    return model
+        if self.trainedBefore:
+            del self.model
+            print("Loading previously trained model")
+            self.model = load_model('partly_trained.h5')
 
-def simulate(envname, steps_, num_rollouts):
+        self.model.fit(X, Y, epochs=epchs, batch_size=bch_size,  verbose=1, validation_split=0.3)
+        self.trainedBefore=1;
+        self.model.save('partly_trained.h5')
+        del self.model
+    def evaluate(self, steps_, num_rollouts, render):
 
-    import gym
-    env = gym.make(envname)
+        self.model = load_model('partly_trained.h5')
 
-    max_steps =  steps_ or env.spec.timestep_limit
-
-    print('loading and building expert policy')
-    policy_fn = load_policy.load_policy("experts/"+envname+".pkl")
-    print('loaded and built')
-
-    with tf.Session():
-        tf_util.initialize()
-
-        state_len, action_len = env_dims(env)
-        observations = np.zeros(shape=(1,state_len))
-        actions = np.zeros(shape=(1,action_len))
+        observations = np.zeros(shape=(1,self.state_len))
+        actions = np.zeros(shape=(1,self.action_len))
+        max_steps =  steps_ or self.env.spec.timestep_limit
 
         for i in range(num_rollouts):
             print('iter', i)
-            obs = env.reset()
+            obs = self.env.reset()
             done = False
             totalr = 0.
             steps = 0
 
             # We start with a set of observation from the env and thus, we go to step 3
-
             while not done:
-                #Step 3: labling the observation by human/expert, which means finding the optimal actions for each obs
-                action = policy_fn(obs[None,:])
-
-                #Step 4: aggregate observation and action data
+                action = self.model.predict(obs[None,:])
                 if steps==0:
                     observations[steps,:]=obs
                     actions[steps,:]=action
                 else:
                     observations=np.vstack((observations, obs))
                     actions=np.vstack((actions, action))
-                ##################### GO BACK TO STEP 1
 
-                #Step1: terrain the NN to learn from these obs and actions
-
-
-                #Step 2: Apply the model to obtain a new a set of observations
-                obs, r, done, _ = env.step(action)
+                obs, r, done, _ = self.env.step(action)
                 totalr += r
                 steps += 1
-                #if args.render:
-                    #env.render()
+                if render:
+                    self.env.render()
                 if steps % 100 == 0: print("%i/%i"%(steps, max_steps))
                 if steps >= max_steps:
                     break
-            return observations, actions
+            print("total reward is ", totalr)
+        return observations, actions, totalr
+
+    def labelData(self, observations):
+
+        print('loading and building expert policy to label Data')
+        policy_fn = load_policy.load_policy("experts/"+self.envname+".pkl")
+        print('loaded and built')
+
+        with tf.Session():
+            tf_util.initialize()
+
+            data_len, _ =observations.shape
+            print("Labling a dataset of length: ", data_len)
+            labels = np.zeros(shape=(1,self.action_len))
+
+            for i in range(data_len):
+                label = policy_fn(observations[i,None])
+                if i==0:
+                    labels[i,:]=label
+                else:
+                    labels=np.vstack((labels, label))
+        print("Lables: ",labels.shape)
+        print("observations: ",observations.shape)
+        return labels
+
 
 
 
@@ -103,14 +117,90 @@ def main():
     parser.add_argument('envname', type=str)
     parser.add_argument('--render', action='store_true')
     parser.add_argument("--max_timesteps", type=int)
+    parser.add_argument("--simulate", type=int, default=0)
     parser.add_argument('--num_rollouts', type=int, default=1,
                         help='Number of expert roll outs')
     args = parser.parse_args()
+    TrainingNum=0
+    if args.simulate==0:  #cloning
+        R=0
+        CB=policy(args.envname)
+        observations, actions= simulate_expert(args.envname,args.max_timesteps,50,0)
+        CB.train(observations,actions,100,100)
+        observations, actions, R = CB.evaluate(args.max_timesteps, 2, 0)
+        print("reward is: ", R)
+    elif args.simulate==1: #run dagger
+        print("Running DAGGER Algorithm")
+        R=0
+        Dagger=policy(args.envname)  #initiate a learning model
+        observations, labeled_action= simulate_expert(args.envname,args.max_timesteps,50,0) #generate human data
 
-    observations, actions= simulate(args.envname,args.max_timesteps,args.num_rollouts)
-    print(actions.shape)
-    
-    model=policy(observations,actions,args.envname)
+        # train on human data
+        Dagger.train(observations,labeled_action,100,100)
+        while R<3500:
+            #apply trained policy and save observation_space
+            observations, _, R = Dagger.evaluate(args.max_timesteps, 2, 0)
+
+            #label observations by human/expert
+            labeled_action=Dagger.labelData(observations)
+
+            # train on labeled data
+            Dagger.train(observations,labeled_action,5,100)
+
+            TrainingNum=TrainingNum+1
+            print("TrainingNum is: ", TrainingNum)
+
+
+
+    elif args.simulate==2: #load the learned model and simulate
+        CB=policy(args.envname,1)
+        CB.evaluate(args.max_timesteps, 20, args.render)
+
+
+def simulate_expert(envname, steps_, num_rollouts, render=1):
+
+    #import gym
+    env = gym.make(envname)
+
+    max_steps =  steps_ or env.spec.timestep_limit
+
+    print('loading and building expert policy for data generation')
+    policy_fn = load_policy.load_policy("experts/"+envname+".pkl")
+    print('loaded and built')
+
+
+    with tf.Session():
+        tf_util.initialize()
+
+        state_len, action_len = env_dims(env)
+        observations = np.zeros(shape=(1,state_len))
+        actions = np.zeros(shape=(1,action_len))
+
+        for i in range(num_rollouts):
+            obs = env.reset()
+            done = False
+            totalr = 0.
+            steps = 0
+
+            while not done:
+                action = policy_fn(obs[None,:])
+
+                if steps==0:
+                    observations[steps,:]=obs
+                    actions[steps,:]=action
+                else:
+                    observations=np.vstack((observations, obs))
+                    actions=np.vstack((actions, action))
+
+                obs, r, done, _ = env.step(action)
+                totalr += r
+                steps += 1
+                if render:
+                    env.render()
+                if steps >= max_steps:
+                    break
+        return observations, actions
+
 
 
 if __name__ == '__main__':
